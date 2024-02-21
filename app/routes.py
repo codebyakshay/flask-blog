b4cc02a9b406de67375bc886d278ddc916f1d4fc
codebyakshay <admin@codebyakshay.com>
2024-02-21 17:18:59 +0530

    profile-pic feature/compression
    changed css a bit to fit profile pics properly and also added feature to compress profile pic upon update

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 import os
 import secrets
+from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
 from app import app, bcrypt, db
 from app.form import RegistrationForm,LoginForm, UpdateAccountForm
@@ -75,11 +76,18 @@ def logout():
 
 
 def save_picture(form_picture):
+    #renaming files
     random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
     picture_path = os.path.join(app.root_path, 'static/profile-pictures', picture_fn)
-    form_picture.save(picture_path)
+    
+    #compress the image
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)    
+
     return picture_fn
 
 
@@ -88,22 +96,28 @@ def save_picture(form_picture):
 @login_required
 def account():
     form = UpdateAccountForm()
-    if form.validate_on_submit():
-        if form.picture.data:
-            picture_file = save_picture(form.picture.data)
-            current_user.image_file = picture_file
-            
-        current_user.username = form.username.data
-        current_user.email = form.email.data
-        db.session.commit()
-        flash('Account has been updated','success')
-        return redirect(url_for('account'))
-    
-    elif request.method == 'GET':
-        form.username.data = current_user.username
-        form.email.data = current_user.email
+    try:
+        if form.validate_on_submit():
+            if form.picture.data:
+                picture_file = save_picture(form.picture.data)
+                current_user.image_file = picture_file
+                
+            current_user.username = form.username.data
+            current_user.email = form.email.data
+            db.session.commit()
+            flash('Account has been updated','success')
+            return redirect(url_for('account'))
         
-    image_file = url_for('static',filename=f"profile-pictures/{current_user.image_file}")
-    
-    return render_template('account.html', 
-                           title='Account', image_file=image_file, form=form)
+        elif request.method == 'GET':
+            form.username.data = current_user.username
+            form.email.data = current_user.email
+            
+        image_file = url_for('static',filename=f"profile-pictures/{current_user.image_file}")
+        
+        return render_template('account.html', 
+                            title='Account', image_file=image_file, form=form)
+        
+    except Exception as e:
+        db.session.rollback()  # Rollback changes in case of an exception
+        flash(f'An error occurred: {str(e)}', 'danger')
+        return redirect(url_for('account'))
